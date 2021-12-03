ab49cdcd8c51f4dcf2114af43316525b8138e54a
wnddl111 <65129251+wnddl111@users.noreply.github.com>
2021-12-03 11:14:22 +0900

    Update annotation.R
    singleR의 annotation방법 2번째 채택!

--- annotation.R
+++ annotation.R
@@ -1,83 +1,95 @@
-library(SeuratObject)
-library(Seurat)
-library(harmony)
-setwd("/Users/juyoung/Desktop/immunotherapy_jy/singlecell_jy/")
-
-combined=readRDS("./data/harmony_combined_neighbor_0.05.rds")
-cluster_dist=as.data.frame(table(combined$seurat_clusters)) #총 23개 각 cluster별로 1000개씩 랜덤하게 추출
-
-##############
-#data size down
-##############
-#data크기가 1만개가 넘어가는 cell에 대해서만 down시켜줌,, 통계적으로 맞을 거란 생각은 안들지만 
-
-set.seed(1)
-num=0
-for (i in cluster_dist$Freq){
-  if(i>10000){
-    random=sample(x=1:i,size=i*0.1,replace = F)
-    assign(paste0('cluster',num),combined[,which(combined$seurat_clusters==num)][,random])
-  }
-  
-  num=num+1
-}
-#
-
-#1개이상인 cluster0-4번이랑 나머지 merge
-left_combined=combined[,which(combined$seurat_clusters %in% c(5:23))]
-obj_list=c(cluster1,cluster2,cluster3,cluster4,left_combined)
-
-down_combined=merge(cluster0,y=obj_list, project= 'down_combined')
-down_combined#44484개 cell로 줄었땅
-##############
-#basic
-##############
-
-down_combined_test=NormalizeData(down_combined)
-down_combined_test=FindVariableFeatures(down_combined_test, selection.method = 'vst', nfeatures=2000)
-all.genes <- rownames(down_combined_test)
-down_combined_test <- ScaleData(down_combined_test, features =all.genes)
-down_combined_test <- RunPCA(down_combined_test, features = VariableFeatures(object=down_combined_test))
-
-ElbowPlot(down_combined_test)
-down_combined_test <- FindNeighbors(down_combined_test, dims=1:20)
-down_combined_test <- FindClusters(down_combined_test, resolution=0.5)
-down_combined_test <- RunUMAP(down_combined_test, dims = 1:20)
-DimPlot(down_combined_test, reduction = "umap")
-
-down_combined_test$seurat_clusters
-
-table(colnames(down_combined)==colnames(down_combined_test))#all true
-down_combined_test$orig_cluster=down_combined$seurat_clusters
-#test에 ORIG CLUSTER추가해서 그림그릴 때 보여줄 수 있으려나?
 ##############
 #singleR
+#합치는 방법이 총 3갠데 2개를 시도해봤고 최종적으로 2
 ##############
 
 #refernece data가 필요함
 #BiocManager::install('celldex')
 #BiocManager::install('scRNAseq')
+setwd('./singlecell_jy/')
+data("harmony_combined_40000cell")#4만개로 줄인 data사용/ 나중에 server에는 전체 데이터 사용함 
+
 library(celldex)
 library(SingleR)
 library(scRNAseq)
-hpca.se <- HumanPrimaryCellAtlasData()
+
+#####################
+#ref2
+hpca.se <- HumanPrimaryCellAtlasData() #두번째 annotation용 방법
+bpe <- BlueprintEncodeData()#두번째 annotation용 방법
+
+#singleR돌려서 예측한 label확인
+pred.final.down2 <- SingleR(test=final_se, ref=list(BPE=bpe, HPCA=hpca.se), assay.type.ref = 1,
+                            labels= list(bpe$label.main,hpca.se$label.main),
+                            clusters=colLabels(final_se))
+
+table(pred.final.down2$labels)
+table(pred.final.down2$reference) #1 -> 21 / 2 -> 3 1이 이김(bpe) 어떤 ref를 기준으로 annotation이 많이 됐는지 보는 것 
+
+head(pred.final.down2$orig.results$BPE$labels)
+head(pred.final.down2$orig.results$HPCA$labels)
+
+
+plotScoreHeatmap(pred.final.down2)
+plotDeltaDistribution(pred.final.down2)
+save(pred.final.down2, file='./data/pred.final.down2.rda')
+
+new.cluster.ids <- pred.final.down2$labels #예측한 파일 기준으로 ident 바꿈 
+names(new.cluster.ids) <-levels(final)
+final <- RenameIdents(final, new.cluster.ids)
+
+save(final, file='./data/final_singleR_annotation2.rda')
+
+
+#####################
+#ref1
+hpca2 <- hpca.se
+hpca2$label.main <- paste0('HPCA.', hpca2$label.main) #어느 ref에서 온건지 헷갈리지 않으려고 설정하는 것 
+
+bpe2 <- bpe
+bpe2$label.main <- paste0('BPE.', bpe2$label.main)
+
+shared <- intersect (rownames(hpca2), rownames(bpe2))
+length(shared) #16267
+dim(hpca2)#19363
+dim(bpe2)#19859
+
+union <- cbind(hpca2[shared,], bpe2[shared,])
 
 #seurat obj가 아니라 singlecellexperiment가 input임
-down_combined_test_se=as.SingleCellExperiment(down_combined_test)
-colLabels(down_combined_test_se)=down_combined_test_se$seurat_clusters #이걸통해서 cluster별로 지정할 수 있음 
-#assay.type.ref = An integer scalar or string specifying the assay of ref containing the relevant expression matrix
-pred.down.combined_test <-SingleR(test=down_combined_test_se, ref=hpca.se, assay.type.ref = 1,
-                             labels= hpca.se$label.main,
-                             clusters=colLabels(down_combined_test_se))
-pred.down.combined_test.table<-as.data.frame(pred.down.combined_test) #각 row는 single cell에 대한 예측결과, first.label이 미세 조정 전 결과, label이  조정 후 결과 
-table(pred.down.combined_test$labels)
-a=as.data.frame(down_combined_test$seurat_clusters,down_combined_test$orig_cluster)
-View(a)
-new.cluster.ids <- pred.down.combined_test$labels
-#names(new.cluster.ids) <-levels(down_combined_test)
-#down_combined_test <- RenameIdents(down_combined_test, new.cluster.ids)
-Idents(down_combined_test)=new.cluster.ids
-DimPlot(down_combined_test, reduction = 'umap', label=T, pt.size = 0.5)
 
-pred.down.combined
+final_se=as.SingleCellExperiment(final)
+colLabels(final_se)=final_se$seurat_clusters #이걸통해서 cluster별로 지정할 수 있음 
+
+
+#assay.type.ref = An integer scalar or string specifying the assay of ref containing the relevant expression matrix
+pred.final.down <-SingleR(test=final_se, ref=union, assay.type.ref = 1,
+                                  labels= union$label.main,
+                                  clusters=colLabels(final_se))
+pred.final.down.table<-as.data.frame(pred.final.down) #각 row는 single cell에 대한 예측결과, first.label이 미세 조정 전 결과, label이  조정 후 결과 
+table(pred.final.down$labels)
+table(pred.final.down$pruned.labels)#이렇게 통합하면 각 ref에 대한 batch effect가 존재
+#result -> 이렇게 하면 ref별로 따로 naming을 해서 annotation이 어렵고
+#the marker set is likely to contain genes responsible for uninteresting batch effects
+#cell type이 아니라 batch effect에 의해서 차이나는 유전자가 있을 수 있다 그말이래 
+
+
+##############
+##############
+
+getwd()
+png("./figure/singleR_annotation2.png",width=1000)
+p1 <- DimPlot(final, reduction = 'umap', label=T, pt.size = 0.5)
+p1#그림이 안보이는 거지 그려짐 ! 저장됨 
+dev.off()
+
+png("./figure/singleR_annotation_cluster2.png", width=1000)
+p1 <- DimPlot(final,group.by = 'seurat_clusters', reduction = 'umap', label=T, pt.size = 0.5)
+p1#그림이 안보이는 거지 그려짐 ! 저장됨 
+dev.off()
+
+#singleR_annotation.pdf -> singleR돌려서 cluster name 대신에 cell type으로 annotation한 결과
+#singleR_annotation_cluster -> 비교를 위해서 cluster name으로 annotation한 거, labeling함
+#singleR_annotation_cluster_F-> labeling 안함, 겹치는 게 있어서 
+#끝에 2 붙은 것들은 이제 HPCA+BPE 2번째 방법으로 합쳤을 때 결과임
 

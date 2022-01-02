0c7eba3264180fc2024544d937cf9e71bdcec0d9
wnddl111 <65129251+wnddl111@users.noreply.github.com>
2022-01-03 00:48:31 +0900

    make_seurat_from_cellranger
    위암, 간암 raw 데이터에 대해서 /juyoung/scRNAseq/scRNAseq2.py로 cell ranger 코드를 돌리고 
    이 파일로 seuat obj로 합쳤다.

---
+++ gse115978_for_local.R
@@ -0,0 +1,44 @@
+library(Seurat)
+
+sample_list_gastric=read.table(file='/data/juyoung/scRNAseq/immunotherapy_gastric/run_cellranger_count/for_R_samplelist_name.txt')
+sample_list_liver=read.table(file='/data/juyoung/scRNAseq/immunotherapy_liver/run_cellranger_count/for_R_samplelist_name.txt')
+
+sample_list_gastric <- as.list(sample_list_gastric$V1)
+sample_list_liver <- as.list(sample_list_liver$V1)
+
+dir_name='run_cellranger_count'
+project_name='immunotherapy_gastric'
+
+setwd('/data/juyoung/scRNAseq/')
+
+for (i in sample_list_gastric){
+  assign(paste0(i,'.data'),Read10X(data.dir=paste0('./',project_name,'/',dir_name,'/run_count_',i,'/outs/filtered_feature_bc_matrix')))
+  assign(i, CreateSeuratObject(counts=get(paste0(i,'.data')),project = i,min.cells = 3, min.features = 200))
+  seurat=get(i)
+  saveRDS(seurat,file=paste0('/data/juyoung/scRNAseq/for_local/gastric_',i,'.rds'))
+}
+
+project_name='immunotherapy_liver'
+for (i in sample_list_liver){
+  print(i)
+  assign(paste0(i,'.data'),Read10X(data.dir=paste0('./',project_name,'/',dir_name,'/run_count_',i,'/filtered_feature_bc_matrix')))
+  assign(i, CreateSeuratObject(counts=get(paste0(i,'.data')),project = i,min.cells = 3, min.features = 200))
+  seurat=get(i)
+  saveRDS(seurat,file=paste0('/data/juyoung/scRNAseq/for_local/liver_',i,'.rds'))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

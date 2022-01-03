028e482471d17471f3327cfa0da9479df7b37a80
wnddl111 <65129251+wnddl111@users.noreply.github.com>
2022-01-03 15:12:04 +0900

    Update immunotherapy.R
    로컬이랑 서버랑 왔다갔다 해야하는 게 있어서 최종적으로 정리

--- immunotherapy.R
+++ immunotherapy.R
@@ -2,13 +2,7 @@ library(Seurat)
 library(SeuratObject)
 setwd('/Users/juyoung/Desktop/immunotherapy_jy/singlecell_jy')
 ################
-#save(gse115978_cell_annotations,file='./data/gse115978_cell_annotations.rda')
-#save(gse115978_counts,file='./data/gse115978_counts.rda')
-#save(gse123813_bcc_metadata,file='./data/gse123813_bcc_metadata.rda')
-#save(gse123813_bcc_counts,file='./data/gse123813_bcc_counts.rda')
-#save(gse123813_scc_counts,file='./data/gse123813_scc_counts.rda')
-#save(gse123813_scc_metadata,file='./data/gse123813_scc_metadata.rda')
-################
+#local
 #################
 ####gse115978####
 #melanoma - post data 만 사용함 
@@ -124,6 +118,7 @@ table(test_use_rawcount[-which(rownames(test_use_rawcount) %in% split_su004FUT_g
 #################
 #last gse123813##
 #################
+#project별로 통째로 진행 
 gse123813_bcc <-CreateSeuratObject(counts= gse123813_bcc_counts, project = 'gse123813_bcc',min.cells = 3, min.features = 200)
 #
 gse123813_scc_counts=read.csv(file='/Users/juyoung/Downloads/GSE123813_scc_scRNA_counts.txt',header = T,row.names = 1,sep='\t')
@@ -181,53 +176,54 @@ Idents(gse123813_scc)=gse123813_scc$sample
 
 ###############
 ###위암,간암###
+#server
 ###############
 
-#server - /homw/juyoung/scRNAseq/gse11~fol_local 이걸로 seuratobject다 만듬 -> 다시 local로 옮김 
-#seurat object는 각각 rda 파일로 만들어서 /dat/juyoung/scRNAseq에 있음 (그냥 ./for_local만들고 안으로 집어넣음)
-#rds파일로 저장 
-#하나씩 치기 귀찮아서 command line 서버에서 만든거 가져옴(r_command.txt)
-gastric_1058BP=readRDS('./data/gastric_1058BP.rds')
-gastric_1058BT=readRDS('./data/gastric_1058BT.rds')
-gastric_1058FUP=readRDS('./data/gastric_1058FUP.rds')
-gastric_1058FUT=readRDS('./data/gastric_1058FUT.rds')
-gastric_3622BP=readRDS('./data/gastric_3622BP.rds')
-gastric_3622BT=readRDS('./data/gastric_3622BT.rds')
-gastric_3622FUP=readRDS('./data/gastric_3622FUP.rds')
-gastric_3622FUT=readRDS('./data/gastric_3622FUT.rds')
-gastric_39054637BP=readRDS('./data/gastric_39054637BP.rds')
-gastric_39054637BT=readRDS('./data/gastric_39054637BT.rds')
-gastric_39054637FUP=readRDS('./data/gastric_39054637FUP.rds')
-gastric_39054637FUT=readRDS('./data/gastric_39054637FUT.rds')
-gastric_40784154BP=readRDS('./data/gastric_40784154BP.rds')
-gastric_40784154BT=readRDS('./data/gastric_40784154BT.rds')
-gastric_40784154FUP=readRDS('./data/gastric_40784154FUP.rds')
-gastric_40784154FUT=readRDS('./data/gastric_40784154FUT.rds')
-gastric_7727BP=readRDS('./data/gastric_7727BP.rds')
-gastric_7727BT=readRDS('./data/gastric_7727BT.rds')
-gastric_7727FUP=readRDS('./data/gastric_7727FUP.rds')
-gastric_7727FUT=readRDS('./data/gastric_7727FUT.rds')
-liver_10665612BP=readRDS('./data/liver_10665612BP.rds')
-liver_10665612FUP=readRDS('./data/liver_10665612FUP.rds')
-liver_12375821BP=readRDS('./data/liver_12375821BP.rds')
-liver_12375821FUP=readRDS('./data/liver_12375821FUP.rds')
-liver_22448359BP=readRDS('./data/liver_22448359BP.rds')
-liver_22448359FUP=readRDS('./data/liver_22448359FUP.rds')
-liver_32982982BP=readRDS('./data/liver_32982982BP.rds')
-liver_32982982FUP=readRDS('./data/liver_32982982FUP.rds')
-liver_36335616BP=readRDS('./data/liver_36335616BP.rds')
-liver_36335616FUP=readRDS('./data/liver_36335616FUP.rds')
-liver_38949491BP=readRDS('./data/liver_38949491BP.rds')
-liver_38949491FUP=readRDS('./data/liver_38949491FUP.rds')
-liver_39745926BP=readRDS('./data/liver_39745926BP.rds')
-liver_39745926FUP=readRDS('./data/liver_39745926FUP.rds')
-liver_40183243BP=readRDS('./data/liver_40183243BP.rds')
-liver_40183243FUP=readRDS('./data/liver_40183243FUP.rds')
-liver_40218172BP=readRDS('./data/liver_40218172BP.rds')
-liver_40218172FUP=readRDS('./data/liver_40218172FUP.rds')
-liver_41226138BP=readRDS('./data/liver_41226138BP.rds')
-liver_41226138FUP=readRDS('./data/liver_41226138FUP.rds')
+setwd('/data/juyoung/scRNAseq/for_local/') #얘는 위치가 여기임 
+gastric_1058BP=readRDS('./gastric_1058BP.rds')
+gastric_1058BT=readRDS('./gastric_1058BT.rds')
+gastric_1058FUP=readRDS('./gastric_1058FUP.rds')
+gastric_1058FUT=readRDS('./gastric_1058FUT.rds')
+gastric_3622BP=readRDS('./gastric_3622BP.rds')
+gastric_3622BT=readRDS('./gastric_3622BT.rds')
+gastric_3622FUP=readRDS('./gastric_3622FUP.rds')
+gastric_3622FUT=readRDS('./gastric_3622FUT.rds')
+gastric_39054637BP=readRDS('./gastric_39054637BP.rds')
+gastric_39054637BT=readRDS('./gastric_39054637BT.rds')
+gastric_39054637FUP=readRDS('./gastric_39054637FUP.rds')
+gastric_39054637FUT=readRDS('./gastric_39054637FUT.rds')
+gastric_40784154BP=readRDS('./gastric_40784154BP.rds')
+gastric_40784154BT=readRDS('./gastric_40784154BT.rds')
+gastric_40784154FUP=readRDS('./gastric_40784154FUP.rds')
+gastric_40784154FUT=readRDS('./gastric_40784154FUT.rds')
+gastric_7727BP=readRDS('./gastric_7727BP.rds')
+gastric_7727BT=readRDS('./gastric_7727BT.rds')
+gastric_7727FUP=readRDS('./gastric_7727FUP.rds')
+gastric_7727FUT=readRDS('./gastric_7727FUT.rds')
 
+
+liver_10665612BP=readRDS('./liver_10665612BP.rds')
+liver_10665612FUP=readRDS('./liver_10665612FUP.rds')
+liver_12375821BP=readRDS('./liver_12375821BP.rds')
+liver_12375821FUP=readRDS('./liver_12375821FUP.rds')
+liver_22448359BP=readRDS('./liver_22448359BP.rds')
+liver_22448359FUP=readRDS('./liver_22448359FUP.rds')
+liver_32982982BP=readRDS('./liver_32982982BP.rds')
+liver_32982982FUP=readRDS('./liver_32982982FUP.rds')
+liver_36335616BP=readRDS('./liver_36335616BP.rds')
+liver_36335616FUP=readRDS('./liver_36335616FUP.rds')
+liver_38949491BP=readRDS('./liver_38949491BP.rds')
+liver_38949491FUP=readRDS('./liver_38949491FUP.rds')
+liver_39745926BP=readRDS('./liver_39745926BP.rds')
+liver_39745926FUP=readRDS('./liver_39745926FUP.rds')
+liver_40183243BP=readRDS('./liver_40183243BP.rds')
+liver_40183243FUP=readRDS('./liver_40183243FUP.rds')
+liver_40218172BP=readRDS('./liver_40218172BP.rds')
+liver_40218172FUP=readRDS('./liver_40218172FUP.rds')
+liver_41226138BP=readRDS('./liver_41226138BP.rds')
+liver_41226138FUP=readRDS('./liver_41226138FUP.rds')
+########################
+#20220102 위암이랑 간암도 sample별로 할 때랑 project로 할 때 소실이 컸음... 그래서 다시 돌리고 진행함 
 ########################
 gastric_name=list('gastric_1058BP','gastric_1058BT','gastric_1058FUP','gastric_1058FUT','gastric_3622BP','gastric_3622BT','gastric_3622FUP','gastric_3622FUT','gastric_39054637BP','gastric_39054637BT',
                   'gastric_39054637FUP','gastric_39054637FUT','gastric_40784154BP','gastric_40784154BT','gastric_40784154FUP','gastric_40784154FUT','gastric_7727BP','gastric_7727BT','gastric_7727FUP','gastric_7727FUT')
@@ -236,9 +232,15 @@ for (i in gastric_name){
   object_list=append(object_list,get(i))
 }
 
+gene=rownames(gastric_1058BP)
+for( i in object_list){
+  print(table(gene==rownames(i)))
+} #all true 예상대로 feature 모두 같음 
+
 sample_list=list('1058BP','1058BT','1058FUP','1058FUT','3622BP','3622BT','3622FUP','3622FUT', '39054637BP','39054637BT','39054637FUP','39054637FUT','40784154BP','40784154BT', '40784154FUP','40784154FUT', '7727BP','7727BT','7727FUP','7727FUT')
-#length(sample_list) 
+#length(sample_list)) 
 assign('gastric', merge(object_list[[1]],y=object_list[2:20], add.cell.ids =sample_list, project= 'gastric'))
+gastric #36601 214946 
 ########################
 liver_name=list('liver_10665612BP',
                 'liver_10665612FUP',
@@ -265,6 +267,11 @@ for (i in liver_name){
   object_list=append(object_list,get(i))
 }
 
+gene=rownames(liver_41226138FUP)
+for( i in object_list){
+  print(table(gene==rownames(i)))
+} #all true 예상대로 feature 모두 같음 
+
 sample_list=list('10665612BP',
                  '10665612FUP',
                  '12375821BP',
@@ -287,33 +294,31 @@ sample_list=list('10665612BP',
                  '41226138FUP')
 assign('liver', merge(object_list[[1]],y=object_list[2:20], add.cell.ids =sample_list, project= 'liver'))
 
+g1<-CreateSeuratObject(counts=gastric@assays$RNA@counts, project='gastric', min.cells = 3, min.features = 200)
+g1 #28050 200781 
 
-###########
-###batch###
-###########
-#1.firm
-#window는 됨 -> 집에서 해보자
-###########
-#2.seurat integration -> batch보정이 더 되야함..
-#10x와 smartseq 우선은 platform끼리 merge하지말고 구분할 컬럼을 만들어서 해보자
-gastric$platform=rep('10x',ncol(gastric)) #g1
-liver$platform=rep('10x',ncol(liver))#l1 
+l1<-CreateSeuratObject(counts=liver@assays$RNA@counts, project='liver', min.cells = 3, min.features = 200)
+l1 #20496 37679
+
+g1$platform=rep('10x',ncol(gastric)) #g1
+l1$platform=rep('10x',ncol(liver))#l1 
 mel$platform=rep('Smartseq2',ncol(melanoma)) #ㅎgse115978_fu를 했어야 함 
 bcc$platform=rep('10x',ncol(bcc))
 scc$platform=rep('10x',ncol(scc))
 
 #test용이라 생각하고 복사하자
-g1=gastric
-l1=liver
 bcc=gse123813_bcc
 scc=gse123813_scc
 mel= gse115978_fu
 
+setwd('/data/juyoung/scRNAseq/for_local/immunotherapy_jy/singlecell_jy')
 #save(mel,file='./data/mel.rda')
 #save(bcc,file='./data/bcc.rda')
 #save(scc,file='./data/scc.rda')
+#save(g1,file='./data/g1.rda')
+#save(l1,file='./data/l1.rda')
+
 #####again########
-setwd('/Users/juyoung/Desktop/immunotherapy_jy/singlecell_jy')
 data(mel)
 data(g1)
 data(bcc)
@@ -400,172 +405,8 @@ scc <- subset(scc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.m
 # Visualize QC metrics as a violin plot
 VlnPlot(g1, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
 
-#saveRDS(l1,file='./l1_last.rds')
-#saveRDS(g1,file='./g1_last.rds')
-#saveRDS(bcc,file='./bcc_last.rds')
-#saveRDS(scc,file='./scc_last.rds')
-#saveRDS(mel,file='./mel_last.rds')
-
-l1=readRDS('./l1_last.rds')
-g1=readRDS('./g1_last.rds')
-bcc=readRDS('./bcc_last.rds')
-scc=readRDS('./scc_last.rds')
-mel=readRDS('./mel_last.rds')
-
-setwd('/Users/juyoung/Desktop/immunotherapy_jy/singlecell_jy/')
-#normalize, find varable features
-seurat_intergrate_list=list(g1,l1,bcc,scc,mel)
-seurat_intergrate_list <- lapply(X=seurat_intergrate_list, FUN=function(x){
-  x <-NormalizeData(x)
-  x <-FindVariableFeatures(x,selection.method='vst',nfeatures=2000)
-})
-
-features <- SelectIntegrationFeatures(object.list = seurat_intergrate_list)
-
-#anchors <- FindIntegrationAnchors(object.list=seurat_intergrate_list,anchor.features =features)
-#combined <- IntegrateData(anchorset = anchors)
-#2시간넘게 도는 중 -> 서버로옮김
-setwd("/Users/juyoung/Desktop/singlecell_jy/data")
-combined=readRDS('./combined.rds')#207657
-
-DefaultAssay(combined) <-'integrated'
-b=lapply(combined$platform,function(x)ifelse(x=='10x','10x','Smartseq2'))
-combined$platform=NULL
-combined$plaform=b
-#defaultassay를 integrated로 설정하거나 rna로 할 수 있음
-#여기서 integrated는 합쳐진 value를 가지고 분석을 한다는 거소
-#rna는 original value에서 분석을 한다는 거다 
-#https://www.biostars.org/p/399789/
-combined <-ScaleData(combined, verbose=F)
-combined <-RunPCA(combined, npcs=30, verbose=F)
-combined <-RunUMAP(combined, reduction = 'pca',dims=1:30)
-combined <-FindNeighbors(combined, reduction = 'pca',dims=1:30)
-combined <-FindClusters(combined, resolution = 0.1)
-
-p1 <- DimPlot(combined,reduction='umap',group.by ='sample',raster=F,size=0.05)
-p2 <- DimPlot(combined, reduction = 'umap',label=T,repel=T,raster=F)
-p1
-
-#b=colnames(combined[,which(combined$cancer_type=='melanoma')])
-#combined$platform='10x'
-table(combined$platform)
-#combined$platform[b]='Smartseq2'
-
-#########################################################
-universe <-rownames(gse115978)
-for (gastric in sample_list_gastric){
-  universe=intersect(universe,rownames(get(gastric)))
-}
-for (liver in sample_list_liver){
-  universe=intersect(universe,rownames(get(liver)))
-}
-universe=intersect(universe, rownames(gse123813))
-length(universe) #5747
-
-##########
-##########
-all_list = append(sample_list_gastric,sample_list_liver)
-all_list[[41]]='gse115978'
-all_list[[42]]='gse123813'
-
-#subset
-for ( i in all_list){
-  assign(i,get(i)[universe,])
-}
-
-#누가 유전자 개수 줄이는 데 크게 기여했는지 보려고 
-for (i in all_list){
-  print(length(rownames(get(i))))
-}
-
-library(devtools)
-devtools::install_github('satijalab/seurat-data',force=T)
-devtools::install_github('satijalab/seurat-wrappers')
-library(ggplot2)
-library(patchwork)
-library(SeuratWrappers)
-
-###########
-#3. harmony (firm, seurat 다음으로 integration잘함)
-#install.packages('harmony')
-library(harmony)
-union_list=c(l1,bcc,scc,mel)
-
-u <- rownames(g1)
-for(i in union_list){
-  u <- union(u,rownames(i))
-}
-
-c <- colnames(g1)
-for(i in union_list){
-  c <- union(c,colnames(i))
-}
-
-integrated_counts <- matrix(0, length(u), ncol(g1)+ncol(l1)+ncol(mel)+ncol(bcc)+ncol(scc))
-rownames(integrated_counts) <-u
-colnames(integrated_counts) <-c
-
-print(object.size(integrated_counts), unit="GB")
-integrated_counts[rownames(g1),colnames(g1)] <-as.matrix(g1@assays$RNA@counts)
-
-g3=subset(g1,orig.ident %in% c('39054637FUP','39054637FUT','40784154BP','40784154BT', '40784154FUP','40784154FUT', '7727BP','7727BT','7727FUP','7727FUT'))
-g2 = subset(g1, orig.ident %in% c('1058BP','1058BT','1058FUP','1058FUT','3622BP','3622BT','3622FUP','3622FUT', '39054637BP','39054637BT'))
-a=as.matrix(GetAssayData(g2))
-
-b=GetAssayData(g3)
-table(rownames(a)==rownames(b)) #all true
-c=cbind(a,b)
-class(c)#여기서 matrix로 ㅁ바꾸는 거 안됨
-
-
-sample_list=list('1058BP','1058BT','1058FUP','1058FUT','3622BP','3622BT','3622FUP','3622FUT', '39054637BP','39054637BT','39054637FUP','39054637FUT','40784154BP','40784154BT', '40784154FUP','40784154FUT', '7727BP','7727BT','7727FUP','7727FUT')
-length(sample_list)
-n_list=c('1058','3622','39054637','40784154','7727')
-subset(g1, orig.ident %in% sample_list[grep('1058',sample_list)])
-
-
-for (i in n_list){
-  assign(i,as.matrix(GetAssayData(subset(g1, orig.ident %in% sample_list[grep(i,sample_list)]))))
-}
-z=cbind(get('1058'),get('3622'),get('39054637'),get('40784154'),get('7727'))
-class(z)
-z[1:2,1:2]
-
-prep_data(z)
-
-gastric_count_matrix=z
-setwd('./firm/data/')
-save(gastric_count_matrix,file='./gastric_count_matrix.rda')
-
-g1[,which(gq$orig.ident=='1058BP')]
-
-sparsity(g1@assays$RNA@counts) #sparse를 dense넣으려니까 오류 -> 이러면 좀 버리고 해야한다는데 버리는 게 좀 그렇...잔하ㅏㅏ.
-#https://stackoverflow.com/questions/58302449/what-does-the-cholmod-error-problem-too-large-means-exactly-problem-when-conv
-
-integrated_counts[rownames(g2),colnames(g2)] <-as.matrix(g2@assays$RNA@counts)
-
-##############
-##############
-test = data.frame(GetAssayData(g2))
-test2 = data.frame(GetAssayData(g2))
-final = merge(test,test2)
-l1$type
-
-basename(getwd())
-readLines(root$find_file("DESCRIPTION"),3)
-root <- rprojroot::is_r_package
-library(rprojroot)
-dir(find_root(has_file("DESCRIPTION")))
-
-devtools::install_github("mingjingsi/FIRM")
-devtools::load_all()
-pkgbuild::check_build_tools(debug = TRUE)
-library(devtools)
-library(Seurat)
-library(RANN)
-library(Rcpp)
-install.packages('usethis')
-install.packages('roxygen2')
-install.packages('testthat')
-usethis::proj_set(getwd())
-
+#saveRDS(l1,file='./data/l1_last.rds')
+#saveRDS(g1,file='./data/g1_last.rds')
+#saveRDS(bcc,file='./data/bcc_last.rds')
+#saveRDS(scc,file='./data/scc_last.rds')
+#saveRDS(mel,file='./data/mel_last.rds')

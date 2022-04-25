dc9549a4e4ee42c9cbe5aa824564e54de48b9a81
wnddl111 <65129251+wnddl111@users.noreply.github.com>
2022-04-25 12:30:16 +0900

    Update 0327_final_server_crpc.R
    normal sample에서 튀는 값이 있어서 하나 제거함..ㅎ.ㅎ

--- 0327_final_server_crpc.R
+++ 0327_final_server_crpc.R
@@ -6,14 +6,15 @@ head(f_merge_gene)
 
 T_sample = colnames(f_merge_gene)[1:36]
 
-#extract 56 people from gtex data and merge t sample
-54+36 #90
+#미래에서 왔다 normal에서 하나느 제거 한다 u3zg
+sample[25]
+sample=sample[-25]
 
-f_merge_gene_90 = f_merge_gene[, c(T_sample,sample)]
+f_merge_gene_89 = f_merge_gene[, c(T_sample,sample)]
 
-dim(f_merge_gene_90)#36506 90
+dim(f_merge_gene_89)#36506 89
 
-cnt.zero = rowSums(f_merge_gene_90==0)
+cnt.zero = rowSums(f_merge_gene_89==0)
 table(cnt.zero)
 hist(cnt.zero)
 
@@ -34,7 +35,7 @@ preprocess_deg <- function(zero_threshold, count){
   message('zero_count_dim is')
   print(dim(zero_count))
   
-  type = c(rep('T',36),rep('N',54))
+  type = c(rep('T',36),rep('N',53))
   sample =colnames(zero_count)
   
   feature = as.data.frame(cbind(sample,type))
@@ -67,8 +68,8 @@ DEG <- function(zero_count, coldata){
 }
 
 for (i in c(0.1,0.3,0.5,1)){ 
-  R = preprocess_deg(i, f_merge_gene_90)
-  colnames(f_merge_gene_90)
+  R = preprocess_deg(i, f_merge_gene_89)
+  colnames(f_merge_gene_89)
   zero = as.data.frame(R[1])
   
   col = as.data.frame(R[3])
@@ -77,7 +78,7 @@ for (i in c(0.1,0.3,0.5,1)){
   
   feature= as.data.frame(R[2])
   
-  write.table(zero,file=paste0('./data/F0405_preprocess_zero',i,'.txt'), sep='\t')
+  write.table(zero,file=paste0('./data/89_F0405_preprocess_zero',i,'.txt'), sep='\t')
   
   D=DEG(zero, feature)
   colnames(zero) == rownames(feature)
@@ -85,12 +86,12 @@ for (i in c(0.1,0.3,0.5,1)){
   vst=as.data.frame(D[2]) #test
   colnames(vst) = col$c..01_expected_count....04_expected_count....08_expected_count...
   dim(vst)
-  write.table(vst, file=paste0('./data/F0405_for_heatmap_',i,'.txt'), sep='\t')
+  write.table(vst, file=paste0('./data/89_F0405_for_heatmap_',i,'.txt'), sep='\t')
   
-  write.table(res_na,file=paste0('./data/F0405_res_',i,'.txt'), sep='\t')
+  write.table(res_na,file=paste0('./data/89_F0405_res_',i,'.txt'), sep='\t')
   
   final_res=res_na[res_na$pvalue<0.05 & abs(res_na$log2FoldChange)>=1.5,]
-  write.table(final_res, file= paste0('./data/F0405_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
+  write.table(final_res, file= paste0('./data/89_F0405_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
   
   up_final_res = final_res[final_res$log2FoldChange >=1.5,]
   down_final_res = final_res[final_res$log2FoldChange <=-1.5,]
@@ -102,20 +103,20 @@ for (i in c(0.1,0.3,0.5,1)){
   print(dim(down_final_res))
   
   
-  write.table(up_final_res, file= paste0('./data/F0405_up_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
-  write.table(down_final_res, file= paste0('./data/F0405_down_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
+  write.table(up_final_res, file= paste0('./data/89_F0405_up_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
+  write.table(down_final_res, file= paste0('./data/89_F0405_down_res_',i,'_p0.05_log2_1.5.txt'), sep='\t')
   
   if (nrow(up_final_res) >=25 && nrow(down_final_res) >=25){
     up_final_res=up_final_res[order(-up_final_res$log2FoldChange),]
     print(head(up_final_res))
     top_25_gene = rownames(up_final_res[1:25,])
-    write.table(rownames(up_final_res[1:30,]), file= paste0('./data/F0405_up_30_',i,'.txt'), sep='\t')
+    write.table(rownames(up_final_res[1:30,]), file= paste0('./data/89_F0405_up_30_',i,'.txt'), sep='\t')
     
     down_final_res=down_final_res[order(down_final_res$log2FoldChange),]
     print(head(down_final_res))
     bottom_25_gene = rownames(down_final_res[1:25,])
     
-    write.table(rownames(down_final_res[1:30,]), file= paste0('./data/F0405_down_30_',i,'.txt'), sep='\t')
+    write.table(rownames(down_final_res[1:30,]), file= paste0('./data/89_F0405_down_30_',i,'.txt'), sep='\t')
   }
   else{
     print('니가 해')
@@ -139,12 +140,19 @@ plot_1 =get('final_50_genes_1')
 
 vst = apply(vst,1,as.numeric)
 hist(vst)
-#boxplot(vst)
+boxplot(vst)
+memory.limit(1000000)
 
 anno = as.data.frame(feature[,'type'])
 rownames(anno) = rownames(feature)
 colnames(anno)[1] = 'type'
 
 range(plot_0.1)
-pheatmap(plot_0.1, cluster_rows = F, cluster_cols = F, annotation_col=anno,
+library(pheatmap)
+pheatmap(plot_1, cluster_rows = F, cluster_cols = F, annotation_col=anno,
          breaks=seq(-3,3,length.out=100), show_colnames = F)
+
+
+
+
+

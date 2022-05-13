6e64749fbc53fd285a847b72a11a59cf2730d6da
wnddl111 <65129251+wnddl111@users.noreply.github.com>
2022-05-13 16:25:52 +0900

    Update immunecellai.R
    normalized fpkm이 젤 잘 동작한대

--- immunecellai.R
+++ immunecellai.R
@@ -1,45 +1,47 @@
+fpkm_only_T = read.table('./data/crpc_T_fpkm_31.txt',sep='\t')
+head(fpkm_only_T)
+dim(fpkm_only_T)
+cnt.zero = rowSums(fpkm_only_T == 0)
+table(cnt.zero)
+hist(cnt.zero)
+
+colnames(fpkm_only_T)=lapply(colnames(fpkm_only_T), function(x) substr(x,2,6))
+
+fbxo_septin14_inc = fpkm_only_T[,as.numeric(substr(colnames(fpkm_only_T),1,2)) %in% c(08,14,25,29,34,15,03,12,39,11,06)]
+fbxo_septin14_noinc = fpkm_only_T[,!as.numeric(substr(colnames(fpkm_only_T),1,2)) %in% c(08,14,25,29,34,15,03,12,39,11,06)]
+
+fpkm_only_T = cbind(fbxo_septin14_inc,fbxo_septin14_noinc)
+dim(fpkm_only_T)
+dim(fbxo_septin14_inc)
+
+#zero
+zero_0.3_fpkm_only_T=fpkm_only_T[-which(cnt.zero > round(ncol(fpkm_only_T)*0.3)),]
+#log2
+range(zero_0.3_fpkm_only_T) # 0 - 587530.6
+log2_zero_0.3_fpkm_only_T = log2(zero_0.3_fpkm_only_T+1)
+range(log2_zero_0.3_fpkm_only_T)# 0 -19.16431
+boxplot(log2_zero_0.3_fpkm_only_T)
+#quantile
+library(preprocessCore)
+norm_log2_zero_0.3_fpkm_only_T=normalize.quantiles(as.matrix(log2_zero_0.3_fpkm_only_T))
+norm_log2_zero_0.3_fpkm_only_T=t(scale(t(norm_log2_zero_0.3_fpkm_only_T)))
+boxplot(norm_log2_zero_0.3_fpkm_only_T)
+hist(apply(norm_log2_zero_0.3_fpkm_only_T,2,as.numeric))
+
 #immunecellai 넣을려고 만드는 파일
-vst = read.table('./data/Fbox25_septin14_for_heatmap_0.3.txt', sep='\t')
-vst = as.data.frame(vst)
-
-data("31_feature")
-
-colnames(vst) = rownames(feature)
-Fbox25_septin14_0.3_immunecellai = rbind(t(feature), vst)
-rownames(Fbox25_septin14_0.3_immunecellai)
+table(is.na(norm_log2_zero_0.3_fpkm_only_T))# all false
+rownames(norm_log2_zero_0.3_fpkm_only_T) = rownames(log2_zero_0.3_fpkm_only_T)
+data("fbxo_septin_feature")
+norm_log2_zero_0.3_fpkm_only_T = as.data.frame(norm_log2_zero_0.3_fpkm_only_T)
+range(norm_log2_zero_0.3_fpkm_only_T)
+colnames(norm_log2_zero_0.3_fpkm_only_T) = rownames(feature)
+Fbox25_septin14_0.3_immunecellai = rbind(t(feature), norm_log2_zero_0.3_fpkm_only_T)
 head(Fbox25_septin14_0.3_immunecellai)
-write.table(Fbox25_septin14_0.3_immunecellai,file='/data2/fusion/fbox25_septin14_immunecellai_0.3.txt',
+
+write.table(Fbox25_septin14_0.3_immunecellai,file='/data2/fusion/fbxo25_septin14_0.3_fpkm.txt',
           sep='\t')
 
 #실제로 돌릴 때 첫번째 열
 #symbol sam1 sam2 sam3 이렇게 되도록 수정
 
 
-
-library(ggplot2)
-fbox25_septin24_immunecellai_result = read.table('./data2/fusion/ImmuCellAI_group_result.txt',sep='\t', header=1)
-head(fbox25_septin24_immunecellai_result)
-
-ggplot(fbox25_septin24_immunecellai_result,
-       aes(fill=))
-
-
-test = read.table('C:/Users/User/Downloads/ImmuCellAI_example.txt',sep='\t', header=1)
-head(test)
-rownames(test) = test$Symbol
-head(test)
-#test = test[-1,-1]
-table(is.na(test))
-
-apply(test,2,range)#log2 한거맞음? 겁나 커
-
-
-data("f_merge_gene_31")
-#f_merge_gene_31
-data("31_feature")
-
-raw_Fbox25_septin14_0.3_immunecellai = rbind(t(feature), f_merge_gene_31)
-rownames(raw_Fbox25_septin14_0.3_immunecellai)
-head(raw_Fbox25_septin14_0.3_immunecellai)
-write.table(raw_Fbox25_septin14_0.3_immunecellai,file='/data2/fusion/fbox25_septin14_immunecellai_raw.txt',
-            sep='\t')
